2047f9e36422d061cc2437a6f64adae8d4b9b102
Otty Praveen <otty.praveen@gmail.com>
2022-06-25 18:50:40 +0530

    This is Commit from Asian timezoneguy

---
+++ src/test/java/Academy/TestCase8.java
@@ -0,0 +1,16 @@
+package Academy;
+
+public class TestCase8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Case A Created");
+		System.out.println("Test Case B Created");
+		System.out.println("Test Case C Created");
+		System.out.println("Test Case D Created");
+		System.out.println("Test Case E Created");
+		
+
+	}
+
+}
