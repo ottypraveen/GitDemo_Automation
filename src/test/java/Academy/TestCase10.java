b1fc797e02afd771726b4e64811ef676f70145e6
Otty Praveen <otty.praveen@gmail.com>
2022-06-25 19:13:25 +0530

    Asian Architect commit

---
+++ src/test/java/Academy/TestCase10.java
@@ -0,0 +1,12 @@
+package Academy;
+
+public class TestCase10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/test/java/Academy/TestCase11.java
@@ -0,0 +1,11 @@
+package Academy;
+
+public class TestCase11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bye Word");
+
+	}
+
+}
